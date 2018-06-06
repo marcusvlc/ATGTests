4d86472eb45996983eb6e257b4d1b18406f56e6b
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2018-06-06 10:50:04 -0300

    Testes para grafos com vertices negativos e alfabeticos

---
+++ src/LibraryTests.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import grafo.*;
+import biblioteca.*;
+
+import org.junit.jupiter.api.Test;
+
+class LibraryTests {
+	
+	public BibliotecaController library;
+	public Graph negativeGraph;
+	
+	@Before
+	public void init() throws Exception {
+
+
+	}
+	
+	/**
+	 * Teste para ler um grafo com vertices negativos
+	 * @throws Exception
+	 */
+	@Test
+	public void testReadNegativeGraph() throws Exception {
+		library = new BibliotecaController();
+		library.readGraph("negativegraph.txt");
+	}
+	
+	@Test
+	public void testAlphabeticGraph() throws Exception {
+		library = new BibliotecaController();
+		library.readGraph("alphabeticgraph.txt");
+	}
+	
+
+}
